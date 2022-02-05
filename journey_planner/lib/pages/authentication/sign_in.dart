6e4ab44bb4fd8aa1921f8e87bdb0906149c47aa5
Djibril Coulybaly <46174096+djibril-coulybaly@users.noreply.github.com>
2022-02-06 08:44:35 +0900

    Relocating sign in page
    I've relocated the sign in page to the authentication folder as opposed to the splash_screen folder it was in previously. I've also created a basic form with fields to enter the email and password with a button to sign in

---
+++ journey_planner/lib/pages/authentication/sign_in.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+import 'package:journey_planner/services/firebase_auth.dart';
+
+class SignIn extends StatefulWidget {
+  final Function toggleView;
+  const SignIn({Key? key, required this.toggleView}) : super(key: key);
+
+  @override
+  State<SignIn> createState() => _SignInState();
+}
+
+class _SignInState extends State<SignIn> {
+  final FAS _auth = FAS();
+  final _formKey = GlobalKey<FormState>();
+  String error = '';
+
+  // text field state
+  String email = '';
+  String password = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.brown[100],
+      appBar: AppBar(
+        backgroundColor: Colors.brown[400],
+        elevation: 0.0,
+        title: const Text('Sign In'),
+        actions: <Widget>[
+          TextButton.icon(
+            icon: const Icon(Icons.person),
+            label: const Text('Register'),
+            onPressed: () => widget.toggleView(),
+          ),
+        ],
+      ),
+      body: Container(
+        padding: const EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: <Widget>[
+              /* Input Email Address */
+              const SizedBox(height: 20.0),
+              TextFormField(
+                validator: (val) => val!.isEmpty ? 'Enter an email' : null,
+                onChanged: (val) {
+                  setState(() => email = val);
+                },
+              ),
+
+              /* Input Password */
+              const SizedBox(height: 20.0),
+              TextFormField(
+                obscureText: true,
+                validator: (val) =>
+                    val!.length < 6 ? 'Enter a password 6+ chars long' : null,
+                onChanged: (val) {
+                  setState(() => password = val);
+                },
+              ),
+
+              /* Sign in button */
+              const SizedBox(height: 20.0),
+              ElevatedButton(
+                  style: ElevatedButton.styleFrom(primary: Colors.pink[400]),
+                  // color: Colors.pink[400],
+                  child: const Text(
+                    'Sign In',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  onPressed: () async {
+                    if (_formKey.currentState!.validate()) {
+                      dynamic result = await _auth.signInUser(email, password);
+                      if (result == null) {
+                        setState(() {
+                          error = 'Could not sign in with those credentials';
+                        });
+                      }
+                    }
+                  }),
+
+              /* Displaying any error messages to the user */
+              const SizedBox(height: 12.0),
+              Text(
+                error,
+                style: const TextStyle(color: Colors.red, fontSize: 14.0),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
