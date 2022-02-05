ff1351e04d294241cf6d34b51371b4ac11a3f263
Djibril Coulybaly <46174096+djibril-coulybaly@users.noreply.github.com>
2022-02-06 08:38:26 +0900

    Update home.dart
    Changes to basic template to demonstrate functionality

--- journey_planner/lib/pages/home/home.dart
+++ journey_planner/lib/pages/home/home.dart
@@ -1,10 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:journey_planner/services/firebase_auth.dart';
 
 class Home extends StatelessWidget {
-  const Home({Key? key}) : super(key: key);
+  Home({Key? key}) : super(key: key);
+  final FAS _auth = FAS();
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.blue[50],
+      appBar: AppBar(
+        title: const Text('Home Dashboard'),
+        backgroundColor: Colors.lightBlue[400],
+        elevation: 0.0,
+        actions: <Widget>[
+          TextButton.icon(
+            icon: const Icon(Icons.person),
+            label: const Text('logout'),
+            onPressed: () async {
+              await _auth.signOut();
+            },
+          ),
+        ],
+      ),
+    );
   }
 }
