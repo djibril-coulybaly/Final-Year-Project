155bc348cc640be267667b5a7c96d62934d3eb25
Djibril Coulybaly <46174096+djibril-coulybaly@users.noreply.github.com>
2022-03-01 10:28:20 +0900

    Update main.dart
    Adding publishableKey for flutter_stripe

--- journey_planner/lib/main.dart
+++ journey_planner/lib/main.dart
@@ -1,13 +1,18 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_stripe/flutter_stripe.dart';
 import 'package:journey_planner/models/user.dart';
 import 'package:journey_planner/pages/firebase_wrapper.dart';
+import 'package:journey_planner/pages/ticket/ticket_overview.dart';
 import 'package:journey_planner/services/firebase_auth.dart';
 import 'package:provider/provider.dart';
 
-void main() async {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+   Stripe.publishableKey =
+      "pk_test_51KSjRkBYgfObXZIJgO2iZSgjKf2mecLP7jDBPe7vcn6EiZRM581fK8c2TjYXdEvbNQhnkMzP8dKeLSPLM3HfQWei00V33ZRrau";
+  await Stripe.instance.applySettings();
   runApp(const MyApp());
 }
 
@@ -22,7 +27,8 @@ class MyApp extends StatelessWidget {
       value: FAS().user,
       initialData: null,
       child: const MaterialApp(
-        home: WrapperForFirebase(),
+        // home: WrapperForFirebase(),
+        home: TicketOverview(),
       ),
     );
   }
