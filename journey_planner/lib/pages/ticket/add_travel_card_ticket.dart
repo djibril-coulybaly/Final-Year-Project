fb939b73cf6cbc5c0b001b547878045b0723f93b
Djibril Coulybaly <46174096+djibril-coulybaly@users.noreply.github.com>
2022-03-01 10:06:54 +0900

    Update add_travel_card_ticket.dart
    Option to choose between "Add Travel Card" and "Add Ticket"

--- journey_planner/lib/pages/ticket/add_travel_card_ticket.dart
+++ journey_planner/lib/pages/ticket/add_travel_card_ticket.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:journey_planner/pages/ticket/add_ticket.dart';
+import 'package:journey_planner/pages/ticket/add_travel_card.dart';
 
 class AddTravelCardOrTicket extends StatefulWidget {
   const AddTravelCardOrTicket({Key? key}) : super(key: key);
@@ -16,7 +18,41 @@ class _AddTravelCardOrTicketState extends State<AddTravelCardOrTicket> {
         elevation: 0.0,
         title: const Text('Add Travel Card or Ticket'),
       ),
-      body: Stack(),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              style: ButtonStyle(
+                foregroundColor: MaterialStateProperty.all<Color>(Colors.blue),
+              ),
+              onPressed: () => Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => AddTravelCard()),
+              ),
+              child: const Text(
+                'Add Travel Card',
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+            ElevatedButton(
+              style: ButtonStyle(
+                foregroundColor: MaterialStateProperty.all<Color>(Colors.blue),
+              ),
+              onPressed: () => Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => AddTicket()),
+              ),
+              child: const Text(
+                'Add Ticket',
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
